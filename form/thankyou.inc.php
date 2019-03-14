9190b70aca822802b5fdf0b38128bd00470393f8
Kelly DeMent <kdement@ufl.edu>
2019-03-14 19:44:41 -0400

    added meta for responsive

--- form/thankyou.inc.php
+++ form/thankyou.inc.php
@@ -449,6 +449,9 @@ header img {
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
 <header>
   <img class="logo" src="images/thank-you.png" alt="Thank You Logo" title="Thank You Logo" />
 
